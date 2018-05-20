a9c4c875d8f8901444ec9bc011b0b223d3bab357
Sauli Nukari <saulinukari@hotmail.com>
2018-05-20 20:47:34 +0300

    initial commit

---
+++ Tyoajanseuranta/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Timers;
+
+namespace Tyoajanseuranta
+{
+    public partial class Form1 : Form
+    {
+        Program.TimerClass firstTimer;
+        Program.TimerClass secondTimer;
+
+        public Form1()
+        {
+            InitializeComponent();
+            firstTimer = new Program.TimerClass();      //formiin timeri program.cs:stä voidaan, lisätä timereitä haluttu määrä
+            secondTimer = new Program.TimerClass();
+        }
+
+        public void setLabel1Text(string text)
+        {
+            label1.Text = text;
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)      //"nimi1" nappi, radio buttonilla valitaan ensin tila ja työntekijä klikkaa sen jälkeen omaa nimeään
+        {
+            if (radioButton1.Checked)
+            {
+                button1.BackColor = Color.LawnGreen;                //jos työmtekijä kirjaa itsensä sisään muuttuu nappi nimen kohdalta vihreäksi.   
+                firstTimer.StartTimer();                            //työaikalaskuri käynistyy.
+            }
+
+            if (radioButton2.Checked)
+            {
+                button1.BackColor = Color.Yellow;                   //työntekijä kirjaa itsensä tauolle, laskuri seisahtuu.
+                firstTimer.StopTimer();
+            }
+
+            if (radioButton3.Checked)
+            {
+                button1.BackColor = Color.White;                    //työntekijä lähtee töistä, laskuri nollaantuu.
+                firstTimer.KillTimer();
+            }
+        }
+
+        private void Clock_Tick(object sender, EventArgs e)
+        {
+            this.label2.Text = DateTime.Now.ToLongTimeString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)      //"nimi2" nappi, radio buttonilla valitaan ensin tila ja työntekijä klikkaa sen jälkeen omaa nimeään
+        {
+            if (radioButton1.Checked)
+            {
+                button2.BackColor = Color.LawnGreen;
+                secondTimer.StartTimer();
+            }
+
+            if (radioButton2.Checked)
+            {
+                button2.BackColor = Color.Yellow;
+                secondTimer.StopTimer();
+            }
+
+            if (radioButton3.Checked)
+            {
+                button2.BackColor = Color.White;
+                secondTimer.KillTimer();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)      //työajan tarkistus, nähdään kuinka kauan kukin henkilö on ollu töissä.
+        {
+            if (textBox1.Text == "nimi1")
+            {
+                label4.Text = ("työaika: " + firstTimer.elapsedTime);
+            }
+
+            else if(textBox1.Text == "nimi2")
+            {
+                label4.Text = ("työaika: " + secondTimer.elapsedTime);                               
+            }
+
+            else
+            {
+                label4.Text = ("syötä nimi1 tai nimi2.");
+            }
+        }
+    }
+
+    
+}
---
+++ Tyoajanseuranta/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Timers;
+
+namespace Tyoajanseuranta
+{
+    static class Program
+    {
+        static Form1 myForm;                                                            //alustetaan Form1 myFormiksi
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            myForm = new Form1();                                                     //luodaan Form1:stä uusi form
+            Application.Run(myForm);                                                  //käsketään ohjelman ajaa "myForm"
+        }
+
+        public class TimerClass                                                       //luodaan ajastimelle luokka
+        {
+            System.Windows.Forms.Timer timer1 = new System.Windows.Forms.Timer();     //luodaan itse timer
+            Stopwatch stopwatch = new Stopwatch();                                    //käytetään stopwatchia timerissä, timeriin tarvittavien ominaisuuksien takia
+
+            public TimeSpan workingtime;                                              //alustetaan working time myöhempää käyttöä varten
+            public string elapsedTime;
+            public void StartTimer()                                                  //timerin aloituksen contruktuuri
+            {
+                timer1.Interval = 1000;                                               //timer interval, eli määritetään kuinka useasti timer tikittää (sekunnin välein)
+                timer1.Enabled = true;                                                // timerin käynnistys komento
+                stopwatch.Start();
+                timer1.Tick += timer1_Tick;                                           //timer jatkaa laskemista ylöspäin
+            }
+            private void timer1_Tick(object sender, EventArgs e)
+            {
+                myForm.setLabel1Text(stopwatch.ElapsedMilliseconds.ToString());       //timerin tikittäessä päivittää joka tikin formiin label1:seen
+            }
+            public void StopTimer()                                                   //Pysäytetään timer
+            {
+                stopwatch.Stop();
+                timer1.Enabled = false;
+            }
+            public void KillTimer()                                                   //timerin sulkeminen kun työntekijä lähtee töistä, tallentaa timerin workingtimeen
+            {
+                workingtime = stopwatch.Elapsed;                                      //asetetaan työaika luettavaan muotoon käyttäjälle
+                elapsedTime = String.Format("{0:00}:{1:00}:{2:00}",
+                workingtime.Hours, workingtime.Minutes, workingtime.Seconds);
+                stopwatch.Reset();
+                timer1.Dispose();
+                myForm.setLabel1Text("bye");
+            }
+        }
+    }
+}
